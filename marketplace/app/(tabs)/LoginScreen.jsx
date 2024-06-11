b5558d25fb49858fce0170a6512c9d39c70c5b17
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-06-11 16:51:18 +0200

    login page in progress

---
+++ marketplace/app/(tabs)/LoginScreen.jsx
@@ -0,0 +1,46 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import * as WebBrowser from "expo-web-browser";
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
+import { useOAuth } from '@clerk/clerk-expo';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function LoginScreen() {
+
+    useWarmUpBrowser();
+
+    const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+    const onPress = React.useCallback(async () => {
+        try {
+            const { createdSessionId, signIn, signUp, setActive } =
+                await startOAuthFlow();
+
+            if (createdSessionId) {
+                setActive({ session: createdSessionId });
+            } else {
+                // Use signIn or signUp for next steps such as MFA
+            }
+        } catch (err) {
+            console.error("OAuth error", err);
+        }
+    }, []);
+
+    return (
+        <View>
+            <Image
+                source={require('./../../assets/images/fond_ciel.jpg')}
+                className="w-full object-cover flex absolute"
+            />
+            <View className="flex flex-column justify-center items-center mt-[300px] mx-5 z-10 p-8 bg-white rounded-3xl shadow-md">
+                <Text className="text-[30px] font-bold">Community Marketplace</Text>
+                <Text className="text-[18px] text-slate-500 mt-6">Buy and sell airsoft gear</Text>
+                <TouchableOpacity onPress={onPress} className="p-4 bg-blue-500 rounded-full mt-20">
+                    <Text className="text-white text-center text-[18px]">Get Started</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    )
+}
--- marketplace/app/(tabs)/index.tsx
+++ marketplace/app/(tabs)/index.tsx
@@ -1,26 +1,20 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Image, StyleSheet, Platform, StatusBar, View, Text } from 'react-native';
+import LoginScreen from './LoginScreen'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Marketplace</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">This is my marketplace app</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ClerkProvider publishableKey='pk_test_aW1tdW5lLWxpb25maXNoLTcyLmNsZXJrLmFjY291bnRzLmRldiQ'>
+      <View>
+        <StatusBar />
+        <SignedIn>
+          <Text style={styles.title}>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   );
 }
 
@@ -41,4 +35,9 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  title: {
+    textAlign: 'center',
+    marginTop: 300,
+
+  }
 });
